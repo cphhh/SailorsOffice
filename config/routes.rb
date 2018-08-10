9b42dd598179682b290ad4a28c46bc2a33b29bfa
Max Bischof <maximilian.bischof@innoz.de>
2018-08-10 19:51:41 +0200

    Add User modell

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  get 'users/new'
 
   root 'static_pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/home', to: 'static_pages#home'
+  get '/signup', to: 'users#new'
+
 end
