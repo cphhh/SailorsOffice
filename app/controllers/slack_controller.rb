cbbf5dcb125ac3b58b086d46865a0a6764eb2835
Max Bischof <maximilian.bischof@innoz.de>
2018-10-29 20:10:43 +0100

    Add response

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -2,6 +2,7 @@ class SlackController < ApplicationController
   protect_from_forgery with: :null_session
 
   def create
-    render :text => "Neue Rechnung erstellt push"
+    message = params['slack']
+    render :plain => "Neue Rechnung erstellt push"
   end
 end
