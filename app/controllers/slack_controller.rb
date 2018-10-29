06734ae1079f1b272554f2b85e7f22dc4967bfe7
Max Bischof <maximilian.bischof@innoz.de>
2018-10-29 22:32:55 +0100

    Add regatta id

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -5,8 +5,7 @@ class SlackController < ApplicationController
     message  = params[:text]
     words = message.split
     render :plain => "Die Rechnung von #{words.second} bei der #{words.first} Regatta über #{words.third}€ wurde erstellt."
-    #regatta = Regatta.select(:id).where(:name => words.first)
-    Invoice.create(regatta_id: 1, user_id: 1, name: words.second, price: words.third)
+    Invoice.create(regatta_id: Regatta.select(:id).where(:name => words.first), user_id: 1, name: words.second, price: words.third)
   end
 
 end
