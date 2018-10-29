fd8e08944f6727d3c6df6aed04ff6b7d0d0a4ff9
Max Bischof <maximilian.bischof@innoz.de>
2018-10-29 21:54:08 +0100

    Add invoice new

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -5,6 +5,8 @@ class SlackController < ApplicationController
     message  = params[:text]
     words = message.split
     render :plain => "Die Rechnung von #{words.second} bei der #{words.first} Regatta über #{words.third}€ wurde erstellt."
+    regatta = Regatta.id.where(:name => words.first)
+    Invoice.create(regatta_id: regatta, user_id: 1, name: words.second, price: words.third )
   end
 
 end
