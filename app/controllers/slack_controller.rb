5d5af1c10711cb118888dc31be294a21d3b6148c
Max Bischof <maximilian.bischof@innoz.de>
2018-10-30 10:58:51 +0100

    Add id

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -4,8 +4,8 @@ class SlackController < ApplicationController
   def create
     message  = params[:text]
     words = message.split
-    render :plain => "Die Rechnung von #{words.second} bei der #{words.first} Regatta über #{words.third}€ wurde erstellt."
     regattaid = Regatta.where(:name => words.first).select(:id).map &:id
+    render :plain => "Die Rechnung von #{words.second} bei der #{words.first} Regatta über #{words.third}€ wurde erstellt. #{regattaid}"
     Invoice.create(regatta_id: regattaid, user_id: 1, name: words.second, price: words.third)
   end
 
