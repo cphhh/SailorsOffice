eff89a41b5c5c03eab0bcaee2f19f9205a9b0f22
Max Bischof <maximilian.bischof@innoz.de>
2018-11-01 10:58:57 +0100

    Add user and slack names

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -4,9 +4,27 @@ class SlackController < ApplicationController
   def create
     message  = params[:text]
     words = message.split
+    user = params[:user_name]
+
+    if user = "maximilianbischof"
+      user = 1
+    end
+
+    if user = "christian"
+      user = 2
+    end
+
+    if user = "olewittenberg"
+      user = 3
+    end
+
+    if user = "bele.schuett"
+      user = 4
+    end
+
     regattaid = Regatta.where(:name => words.first)[0][:id]
-    render :plain => "Die Rechnung von #{words.second} bei der #{words.first} Regatta über #{words.third}€ wurde erstellt. #{regattaid}"
-    Invoice.create(regatta_id: regattaid, user_id: 1, name: words.second, price: words.third)
+    render :plain => "Die Rechnung von #{words.second} bei der #{words.first} Regatta über #{words.third}€ wurde erstellt."
+    Invoice.create(regatta_id: regattaid, user_id: user, name: words.second, price: words.third)
   end
 
 end
