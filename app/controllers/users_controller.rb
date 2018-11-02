12a0bbdca48765c00fbd3094c779c663034f6417
Max Bischof <maximilian.bischof@innoz.de>
2018-11-02 19:29:26 +0100

    Add loggedin method to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 # Users controller
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:show, :edit, :index, :new, :create, :update, :updaterids]
+
   def show
     @user = User.find(params[:id])
   end
@@ -35,6 +37,13 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+  end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
   end
 
   private
