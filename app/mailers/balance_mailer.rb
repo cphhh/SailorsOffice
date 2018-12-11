4ba7453d7f5d35c30581566c528b190696129440
Max Bischof <maximilian.bischof@innoz.de>
2018-12-11 11:47:42 +0100

    Add send_balance mailer

---
+++ app/mailers/balance_mailer.rb
@@ -0,0 +1,24 @@
+require 'digest/sha2'
+class BalanceMailer < ApplicationMailer
+  default from: 'tinto@bischof.dk'
+  default "Message-ID"=>"#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@bischof.dk"
+
+  def invoice_reminder(regatta, user)
+    @regatta = regatta
+    @user = user
+    mail(to: user.email, subject: "Please submit invoices from #{regatta.name} regatta")
+  end
+
+  def send_balance(regatta, user)
+    @regatta = regatta
+    @user = user
+    @balance = @regatta.balances.first
+    @users = @balance.regatta.users.all
+    @costs = @balance.regatta.invoices.all.sum(:price)
+    @supplement = (((@costs/@users.count)/100)*5).round(2)
+    @fee = ((@balance.regatta.enddate - @balance.regatta.startdate).to_i + 1)*5
+    @totalcosts = ((@costs / @users.count) + @supplement + @fee)
+    @totalprofit = (@users.count*@supplement) + (@users.count*@fee)
+    mail(to: user.email, subject: "Balance for #{regatta.name} regatta")
+  end
+end
---
+++ lib/tasks/send_balance.rake
@@ -0,0 +1,9 @@
+task :send_balance => :environment do
+  Regatta.all.each do |regatta|
+    if regatta.enddate.to_date == 7.days.ago.to_date
+      regatta.users.each do |user|
+        BalanceMailer.send_balance(regatta, user).deliver_now
+      end
+    end
+  end
+end
