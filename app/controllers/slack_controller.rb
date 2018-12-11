809b95cfcda08a91d827d3d94575ae163e2c1f24
Max Bischof <maximilian.bischof@innoz.de>
2018-12-11 15:46:53 +0100

    Fix id

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -6,8 +6,8 @@ class SlackController < ApplicationController
     words = message.split
     user = params[:user_name]
     regatta_name = params[:channel_name]
-    regatta = Regatta.where(name: regatta_name)
-    regattaid = regatta[:id]
+    #regatta = Regatta.where(name: regatta_name)
+
 
     if user == 'maximilianbischof'
       user = 1
@@ -29,7 +29,10 @@ class SlackController < ApplicationController
       user = 5
     end
 
-    render :plain => "Die Rechnung von #{words.first} bei der #{regatta_name} Regatta über #{words.second}€ wurde erstellt.}"
+
+
+    regattaid = Regatta.where(:name => regatta_name)[0][:id]
+    render :plain => "Die Rechnung von #{words.first} bei der #{words.first} Regatta über #{words.second}€ wurde erstellt.}"
     Invoice.create(regatta_id: regattaid, user_id: user, name: words.second, price: words.third)
   end
 
