f8a2a139b96f8afa682539a4fb5016ba275077f3
Max Bischof <maximilian.bischof@innoz.de>
2018-12-11 16:21:53 +0100

    Add feedback to slack when balance is closed

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -6,8 +6,7 @@ class SlackController < ApplicationController
     words = message.split
     user = params[:user_name]
     regatta_name = params[:channel_name]
-    #regatta = Regatta.where(name: regatta_name)
-
+    regatta = Regatta.where(name: regatta_name)
 
     if user == 'maximilianbischof'
       user = 1
@@ -29,11 +28,13 @@ class SlackController < ApplicationController
       user = 5
     end
 
-
-
     regattaid = Regatta.where(:name => regatta_name)[0][:id]
-    render :plain => "Die Rechnung von #{words.first} bei der #{regatta_name} Regatta über #{words.second}€ wurde erstellt.}"
-    Invoice.create(regatta_id: regattaid, user_id: user, name: words.first, price: words.second)
-  end
 
+    if regatta.take.balances.first.closed == true
+      render :plain => "Die Abrechnung für die #{regatta_name} Regatta wurde bereits am #{regatta.take.balances.first.closing_date} geschlossen. Rechnung wurde nicht eingereicht."
+    else
+      Invoice.create(regatta_id: regattaid, user_id: user, name: words.first, price: words.second)
+      render :plain => "Die Rechnung von #{words.first} bei der #{regatta_name} Regatta über #{words.second}€ wurde erstellt.}"
+    end
+  end
 end
