fbc8dce29b900f150868a87f86d280db359403a9
Max Bischof <maximilian.bischof@innoz.de>
2018-12-12 11:40:52 +0100

    Add as helper

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -1,21 +1,6 @@
 class SlackController < ApplicationController
   protect_from_forgery with: :null_session
 
-  def request_verified?
-    timestamp = request.headers["X-Slack-Request-Timestamp"]
-    signature = request.headers["X-Slack-Signature"]
-    signing_secret = ENV["SLACK_SECRET"]
-
-    if Time.at(timestamp.to_i) < 5.minutes.ago
-      return false # expired
-    end
-
-    basestring = "v0:#{timestamp}:#{request.body.read}"
-    my_signature = "v0=#{OpenSSL::HMAC.hexdigest("SHA256", signing_secret, basestring)}"
-
-    ActiveSupport::SecurityUtils.secure_compare(my_signature, signature)
-  end
-
   def create
 
     slack_signing_secret = ENV["SLACK_SECRET"]
--- app/helpers/slack_helper.rb
+++ app/helpers/slack_helper.rb
@@ -1,2 +1,17 @@
 module SlackHelper
+
+  def request_verified?
+    timestamp = request.headers["X-Slack-Request-Timestamp"]
+    signature = request.headers["X-Slack-Signature"]
+    signing_secret = ENV["SLACK_SECRET"]
+
+    if Time.at(timestamp.to_i) < 5.minutes.ago
+      return false # expired
+    end
+
+    basestring = "v0:#{timestamp}:#{request.body.read}"
+    my_signature = "v0=#{OpenSSL::HMAC.hexdigest("SHA256", signing_secret, basestring)}"
+
+    ActiveSupport::SecurityUtils.secure_compare(my_signature, signature)
+  end
 end
