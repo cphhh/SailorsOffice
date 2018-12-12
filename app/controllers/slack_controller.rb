6907513c5f00c8e110096ea2230d7942b43e07e6
Max Bischof <maximilian.bischof@innoz.de>
2018-12-12 11:50:01 +0100

    Add debug infos

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -34,13 +34,28 @@ class SlackController < ApplicationController
 
     regattaid = Regatta.where(:name => regatta_name)[0][:id]
 
-    if request_verified? == true
+    timestamp = request.headers["X-Slack-Request-Timestamp"]
+    signature = request.headers["X-Slack-Signature"]
+    signing_secret = ENV["SLACK_SECRET"]
+
+    #if Time.at(timestamp.to_i) < 5.minutes.ago
+      #return false # expired
+    #end
+
+    basestring = "v0:#{timestamp}:#{request.body.read}"
+    my_signature = "v0=#{OpenSSL::HMAC.hexdigest("SHA256", signing_secret, basestring)}"
+
+    request_verified == ActiveSupport::SecurityUtils.secure_compare(my_signature, signature)
+
+    if request_verified == true
       if Regatta.where(name: regatta_name).take.balances.first.closed == true
         render :plain => "Die Abrechnung für die #{regatta_name} Regatta wurde bereits am #{Regatta.where(name: regatta_name).take.balances.first.closing_date} geschlossen. Rechnung wurde nicht eingereicht."
       elsif Regatta.where(name: regatta_name).take.balances.first.closed == false
         Invoice.create(regatta_id: regattaid, user_id: user, name: words.first, price: words.second)
         render :plain => "Die Rechnung von #{words.first} bei der #{regatta_name} Regatta über #{words.second}€ wurde erstellt. #{request_verified?}"
       end
+    else
+      render :plain => "Request nicht gültig. Request_verified: #{request_verified?}, Timestamp: #{timestamp}, Signature: #{signature}"
     end
   end
 end
