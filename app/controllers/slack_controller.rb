78761d22653595b955e881a087b80258df997440
Max Bischof <maximilian.bischof@innoz.de>
2018-12-12 12:26:59 +0100

    Remove dev helpers

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -40,7 +40,7 @@ class SlackController < ApplicationController
     request_verified = ActiveSupport::SecurityUtils.secure_compare(my_signature, signature)
 
     if Time.at(timestamp.to_i) < 5.minutes.ago
-      request_verified = false 
+      request_verified = false
     end
 
     if request_verified == true
@@ -48,10 +48,10 @@ class SlackController < ApplicationController
         render :plain => "Die Abrechnung für die #{regatta_name} Regatta wurde bereits am #{Regatta.where(name: regatta_name).take.balances.first.closing_date} geschlossen. Rechnung wurde nicht eingereicht."
       elsif Regatta.where(name: regatta_name).take.balances.first.closed == false
         Invoice.create(regatta_id: regattaid, user_id: user, name: words.first, price: words.second)
-        render :plain => "Die Rechnung von #{words.first} bei der #{regatta_name} Regatta über #{words.second}€ wurde erstellt. #{request_verified}"
+        render :plain => "Die Rechnung von #{words.first} bei der #{regatta_name} Regatta über #{words.second}€ wurde erstellt."
       end
     else
-      render :plain => "Request nicht gültig. Request_verified: #{request_verified}, Timestamp: #{timestamp}, Signature: #{signature}"
+      render :plain => "Request nicht gültig."
     end
   end
 end
