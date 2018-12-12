2fae49dc71edee11a8b4baacba8e92c91bce234f
Max Bischof <maximilian.bischof@innoz.de>
2018-12-12 11:29:26 +0100

    Add verify slack request

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -1,7 +1,26 @@
 class SlackController < ApplicationController
   protect_from_forgery with: :null_session
 
+  def request_verified?
+    timestamp = request.headers["X-Slack-Request-Timestamp"]
+    signature = request.headers["X-Slack-Signature"]
+    signing_secret = ENV["SLACK_SECRET"]
+
+    if Time.at(timestamp.to_i) < 5.minutes.ago
+      return false # expired
+    end
+
+    basestring = "v0:#{timestamp}:#{request.body.read}"
+    my_signature = "v0=#{OpenSSL::HMAC.hexdigest("SHA256", signing_secret, basestring)}"
+
+    ActiveSupport::SecurityUtils.secure_compare(my_signature, signature)
+  end
+
   def create
+
+    slack_signing_secret = ENV["SLACK_SECRET"]
+    timestamp = request.headers["X-Slack-Request-Timestamp"]
+
     message  = params[:text]
     words = message.split
     user = params[:user_name]
@@ -30,11 +49,13 @@ class SlackController < ApplicationController
 
     regattaid = Regatta.where(:name => regatta_name)[0][:id]
 
-    if Regatta.where(name: regatta_name).take.balances.first.closed == true
-      render :plain => "Die Abrechnung für die #{regatta_name} Regatta wurde bereits am #{Regatta.where(name: regatta_name).take.balances.first.closing_date} geschlossen. Rechnung wurde nicht eingereicht."
-    elsif Regatta.where(name: regatta_name).take.balances.first.closed == false
-      Invoice.create(regatta_id: regattaid, user_id: user, name: words.first, price: words.second)
-      render :plain => "Die Rechnung von #{words.first} bei der #{regatta_name} Regatta über #{words.second}€ wurde erstellt."
+    if request_verified? == true
+      if Regatta.where(name: regatta_name).take.balances.first.closed == true
+        render :plain => "Die Abrechnung für die #{regatta_name} Regatta wurde bereits am #{Regatta.where(name: regatta_name).take.balances.first.closing_date} geschlossen. Rechnung wurde nicht eingereicht."
+      elsif Regatta.where(name: regatta_name).take.balances.first.closed == false
+        Invoice.create(regatta_id: regattaid, user_id: user, name: words.first, price: words.second)
+        render :plain => "Die Rechnung von #{words.first} bei der #{regatta_name} Regatta über #{words.second}€ wurde erstellt. #{request_verified?}"
+      end
     end
   end
 end
