9ada83a27c00bb6f50aa707195de4804a45ee1fe
Max Bischof <maximilian.bischof@innoz.de>
2018-12-20 21:18:16 +0100

    Refactor slack controller

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -2,36 +2,38 @@ class SlackController < ApplicationController
   protect_from_forgery with: :null_session
 
   def create
-    message  = params[:text]
-    words = message.split
+    parameter = params[:text].split
     user = User.where(slack_name: params[:user_name])[0][:id]
-    regatta_name = params[:channel_name]
-    #regatta = Regatta.where(name: regatta_name)
+    regatta = Regatta.where(name: params[:channel_name])[0]
 
-    regattaid = Regatta.where(:name => regatta_name)[0][:id]
+    request_validation
 
-    timestamp = request.headers["X-Slack-Request-Timestamp"]
-    signature = request.headers["X-Slack-Signature"]
-    signing_secret = ENV["SLACK_SECRET"]
-
-    basestring = "v0:#{timestamp}:#{request.body.read}"
-    my_signature = "v0=#{OpenSSL::HMAC.hexdigest("SHA256", signing_secret, basestring)}"
-
-    request_verified = ActiveSupport::SecurityUtils.secure_compare(my_signature, signature)
-
-    if Time.at(timestamp.to_i) < 5.minutes.ago
-      request_verified = false
-    end
-
-    if request_verified == true
-      if Regatta.where(name: regatta_name).take.balances.first.closed == true
+    if valid_request == true
+      if regatta.take.balances.first.closed == true
         render :plain => "Die Abrechnung für die #{regatta_name} Regatta wurde bereits am #{Regatta.where(name: regatta_name).take.balances.first.closing_date} geschlossen. Rechnung wurde nicht eingereicht."
       elsif Regatta.where(name: regatta_name).take.balances.first.closed == false
-        Invoice.create(regatta_id: regattaid, user_id: user, name: words.first, price: words.second)
-        render :plain => "Die Rechnung von #{words.first} bei der #{regatta_name} Regatta über #{words.second}€ wurde erstellt."
+        Invoice.create(regatta_id: regattaid, user_id: user, name: parameter.first, price: parameter.second)
+        render :plain => "Die Rechnung von #{parameter.first} bei der #{regatta_name} Regatta über #{parameter.second}€ wurde erstellt."
       end
     else
       render :plain => "Request nicht gültig."
+    end
+
+    private
+
+    def request_validation
+      timestamp = request.headers["X-Slack-Request-Timestamp"]
+      signature = request.headers["X-Slack-Signature"]
+      signing_secret = ENV["SLACK_SECRET"]
+
+      basestring = "v0:#{timestamp}:#{request.body.read}"
+      my_signature = "v0=#{OpenSSL::HMAC.hexdigest("SHA256", signing_secret, basestring)}"
+
+      valid_request = ActiveSupport::SecurityUtils.secure_compare(my_signature, signature)
+
+      if Time.at(timestamp.to_i) < 5.minutes.ago
+        valid_request = false
+      end
     end
   end
 end
