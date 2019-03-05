69d6d11ba9ba907123d1975bbd1e24047ec548ec
Max <max@bischof.dk>
2019-03-05 15:53:56 +0100

    Add current user balance to root

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,5 +4,8 @@ class StaticPagesController < ApplicationController
 		sixweeksago = Date.today - 42
 		insixweeks = Date.today + 42
     @regattas = Regatta.order(startdate: :asc).where('startdate > ? AND startdate < ?', sixweeksago, insixweeks )
+
+    startamount = 836,26
+    
   end
 end
